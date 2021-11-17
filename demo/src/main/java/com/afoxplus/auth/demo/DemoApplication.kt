7e6fbb18e7993fca8f22357d59daeab12fe27c62
Valentin Mendoza <jmendozat13@gmail.com>
2021-11-17 00:02:07 -0500

    Add package architecture

---
+++ demo/src/main/java/com/afoxplus/auth/demo/DemoApplication.kt
@@ -0,0 +1,8 @@
+package com.afoxplus.auth.demo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DemoApplication : Application() {
+}
--- demo/src/main/java/com/afoxplus/auth/demo/delivery/views/activities/MainActivity.kt
+++ demo/src/main/java/com/afoxplus/auth/demo/delivery/views/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.afoxplus.auth
+package com.afoxplus.auth.demo.delivery.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.afoxplus.auth.demo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
